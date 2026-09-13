5ce669a6b228b48e8e2542e59a6b8d65d8d9ea32
Beda28 <ozekr2007@gmail.com>
2026-09-13 22:45:46 +0900

    feat: 에러 핸들러 추가

---
+++ decorator.py
@@ -0,0 +1,16 @@
+import subprocess
+from functools import wraps
+from google.genai import errors
+
+def handle_error(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except errors.APIError               as e: print(f"GEMINI API 오류: {e}")
+        except errors.ServerError            as e: print(f"GEMINI 서버 오류: {e}")
+        except subprocess.CalledProcessError as e: print(f"명령 실행을 실패했습니다.: {e}")
+        except EnvironmentError              as e: print(f"API_KEY가 설정되지 않았습니다.")
+        except Exception                     as e: print(f"예상하지 못한 오류가 발생했습니다.: {e}")
+        
+    return wrapper
--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import argparse
 
-from prompt import getPrompt
-from util   import copy
+from prompt    import getPrompt
+from util      import copy
+from decorator import handle_error
 
-parser = argparse.ArgumentParser()
+@handle_error
+def main():
+    parser = argparse.ArgumentParser()
 
-parser.add_argument("command",       choices=["commit", "pr"])
-parser.add_argument("--model",       default="gemini-3.6-flash")
-parser.add_argument("--temperature", default=0.3, type=float)
-parser.add_argument("--max-tokens",  default=500, type=int)
+    parser.add_argument("command",       choices=["commit", "pr"], help="실행할 명령어")
+    parser.add_argument("--model",       default="gemini-3.6-flash")
+    parser.add_argument("--temperature", default=0.3, type=float)
+    parser.add_argument("--max-tokens",  default=500, type=int)
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-result = getPrompt(
-    args.command, 
-    args.model,
-    args.temperature, 
-    args.max_tokens )
+    result = getPrompt(
+        args.command, 
+        args.model,
+        args.temperature, 
+        args.max_tokens )
 
-print(result)
-copy(result)
+    print(result)
+    copy(result)
+
+if __name__ == "__main__":
+    main()
