f6d4fa22e9974022514536710d4001bb73d694eb
Beda28 <ozekr2007@gmail.com>
2026-09-13 15:08:18 +0900

    feat: 커밋 메시지 클립보드 복사 기능 추가

--- commit.py
+++ commit.py
@@ -33,3 +33,14 @@ interaction = client.interactions.create(
     input=prompt
 )
 print(interaction.output_text)
+print('\n커밋 메시지를 복사하시겠습니까?')
+if input("복사하시려면 1을 입력해주세요 : ") == "1":
+    subprocess.run(
+        "clip",
+        input=interaction.output_text,
+        text=True,
+        shell=True
+    )
+    print("클립보드에 복사되었습니다!")
+else:
+    print("프로그램 종료")
